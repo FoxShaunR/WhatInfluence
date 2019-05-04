a5af298359b74ce2269b23c8b4a002b1459bafbc
Shaun Fox <shaun@wheelhouse-studio.com>
2019-05-03 17:58:12 -0700

    Move feedparser code to separate file and use compressed example

--- server/app.js
+++ server/app.js
@@ -1,40 +1,5 @@
-const FeedParser = require('feedparser');
-const request = require('request'); // for fetching the feed
-const cron = require('node-cron');
+const { scheduleFeeds } = require('./feedparser');
 
-const feedparser = new FeedParser([]);
+const WATCHED_FEEDS = ['http://kotaku.com/vip.xml', 'http://jezebel.com/vip.xml'];
 
-cron.schedule('* * * * *', () => {
-  const req = request('http://kotaku.com/vip.xml')
-  console.log('running a task every minute');
-
-  req.on('error', function (error) {
-    // handle any request errors
-  });
-  
-  req.on('response', function (res) {
-    const stream = this; // `this` is `req`, which is a stream
-  
-    if (res.statusCode !== 200) {
-      this.emit('error', new Error('Bad status code'));
-    }
-    else {
-      stream.pipe(feedparser);
-    }
-  });
-});
-
-feedparser.on('error', function (error) {
-  // always handle errors
-});
-
-feedparser.on('readable', function () {
-  // This is where the action is!
-  const stream = this; // `this` is `feedparser`, which is a stream
-  const meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-  let item;
-
-  while (item = stream.read()) {
-    console.log(item.title);
-  }
-});
+scheduleFeeds(WATCHED_FEEDS);
