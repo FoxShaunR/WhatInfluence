65ec29e861a6bd256c5ff2973ed26a7e33fb0293
Shaun Fox <shaun@wheelhouse-studio.com>
2019-05-21 18:53:12 -0700

    Add summary and post columns to news

---
+++ server/migrations/20190522014633-news-summary-post-fields.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn('news', 'summary', {
+            type: Sequelize.TEXT
+        }, { transaction: t }),
+        queryInterface.addColumn('news', 'post', {
+          type: Sequelize.JSON
+        }, { transaction: t }),
+      ])
+    })
+  },
+
+down: (queryInterface, Sequelize) => {
+  return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn('news', 'summary', { transaction: t }),
+        queryInterface.removeColumn('news', 'post', { transaction: t }),
+      ])
+    })
+  }
+};
