5269d70b610db1dc1b4d5812f3d2a03405459264
Shaun Fox <shaun@wheelhouse-studio.com>
2019-06-29 18:28:31 -0700

    Add storybook types and remove top level stories

--- content/.storybook/config.js
+++ content/.storybook/config.js
@@ -1,8 +1,6 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../src/stories');
-  // Also add any stories in src/components/**/*.stores.js/tsx
   const req = require.context('../src/components', true, /\/(.+\.)?stories\.(js|tsx)$/)
   req.keys().forEach(filename => req(filename));
 }
