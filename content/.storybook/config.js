578d861fbd5b213f8371bdf8caf90f58792b795b
Shaun Fox <shaun@wheelhouse-studio.com>
2019-06-29 17:44:31 -0700

    Import stories from all component folders

--- content/.storybook/config.js
+++ content/.storybook/config.js
@@ -2,6 +2,9 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories');
+  // Also add any stories in src/components/**/*.stores.js/tsx
+  const req = require.context('../src/components', true, /\/(.+\.)?stories\.(js|tsx)$/)
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
