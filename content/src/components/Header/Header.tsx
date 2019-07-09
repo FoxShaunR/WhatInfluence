ca963675159843f53504a29de706fa8a77248b4f
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-09 08:29:47 -0700

    Switch to SVG logo for header

--- content/src/components/Header/Header.tsx
+++ content/src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Search from './Search';
-import Logo from '../assets/images/Logo.png';
+import Logo from '../assets/images/Logo.svg';
 
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <div className={styles.main}>
-      <img src={Logo} />
+      <img className={styles.logo} src={Logo} />
       <Search />
     </div>
   );
