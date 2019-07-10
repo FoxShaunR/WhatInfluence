bb18e87d470dd8354d183544757a3551d5ba615a
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-09 19:09:27 -0700

    Add react-router-dom and add as story decorator

--- content/.storybook/config.js
+++ content/.storybook/config.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const withRouter = (Comp) => (
+  <Router>
+    <Comp />
+  </Router>
+);
 
 function loadStories() {
   addDecorator(withKnobs);
+  addDecorator(withRouter);
   const req = require.context('../src/components', true, /\/(.+\.)?stories\.(js|tsx)$/)
 
   req.keys().forEach(filename => req(filename));
