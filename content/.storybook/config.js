910d86f98111c51cd4bd7a4abb08566b9be221ee
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-09 18:43:56 -0700

    Add types for stoybook knobs and additional TS config

--- content/.storybook/config.js
+++ content/.storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
 
 function loadStories() {
+  addDecorator(withKnobs);
   const req = require.context('../src/components', true, /\/(.+\.)?stories\.(js|tsx)$/)
+
   req.keys().forEach(filename => req(filename));
 }
 
