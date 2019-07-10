48abe20f32a4a52eb404a55e29787b3beb12a5dc
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-09 19:09:33 -0700

    Add Footer

---
+++ content/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.main}>
+      <Link className={styles.mission} to="/mission">Mission</Link>
+      <Link className={styles.contact} to="/contact">Contact</Link>
+    </div>
+  )
+}
+
+export default Footer
