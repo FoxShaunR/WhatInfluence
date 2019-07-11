5acbd567a58b2e8c0fbf4a3b96b969715c7b31b4
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-10 23:36:27 -0700

    Add initial NewsItem component

--- content/src/components/LatestNews/LatestNews.stories.tsx
+++ content/src/components/LatestNews/LatestNews.stories.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import LatestNews from './LatestNews';
+import NewsItem from './NewsItem';
 
 import { storiesOf } from '@storybook/react';
 
+const NEWS_ITEMS = [
+  {
+    author: 'Joshua Rivera',
+    description: `I’ve been thinking a lot about yesterday’s surprise announcement of Playdate, the new
+    handheld games experiment from Firewatch publisher Panic. More than anything, it’s weird: A small,
+    single-purpose handheld with a set 12-week program of mystery games is the sort of thing that
+    demands your attention. And yes, it has…Read more...`,
+    fullname: 'SOME PERSON',
+    handle: 'InstaPerson',
+    link: 'https://kotaku.com/video-games-should-be-weirder-1834991545',
+    pubdate: new Date('2019-02-02'),
+    source_name: 'Kotaku',
+    title: 'Video Games Should Be Weirder',
+  },
+];
+
 storiesOf('LatestNews', module)
   .add('display', () => <LatestNews />);
+
+storiesOf('LatestNews/NewsItem', module)
+  .add('display', () => <NewsItem {...NEWS_ITEMS[0]} />);
---
+++ content/src/components/LatestNews/NewsItem.tsx
@@ -0,0 +1,50 @@
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+import styles from './NewsItem.module.css';
+
+const formatDate = (date?: Date) => (
+  date ? new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }) : '- -'
+);
+
+interface INewsItem {
+  fullname: string;
+  handle?: string;
+  title: string;
+  description?: string;
+  link: string;
+  author?: string;
+  source_name?: string;
+  pubdate?: Date;
+}
+
+const NewsItem = ({
+  fullname,
+  handle,
+  title,
+  description,
+  link,
+  author,
+  source_name,
+  pubdate,
+}: INewsItem) => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.headerRow}>
+        {/* TODO: update to icon based on source */}
+        <FontAwesomeIcon icon={faNewspaper} size="4x" />
+        <div className={styles.nameGroup}>
+          <div className={styles.name}>{fullname}</div>
+          <div className={styles.handle}>{handle}</div>
+        </div>
+        {<div className={styles.pubdate}>{formatDate(pubdate)}</div>}
+      </div>
+      <div className={styles.title}>{title}</div>
+      {/* TODO: additional styling of name */}
+      <div className={styles.description}>{`${description} - ${source_name}`}</div>
+    </div>
+  );
+};
+
+export default NewsItem;
