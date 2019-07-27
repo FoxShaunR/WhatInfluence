a35c9143cb86704b1c4b1416ee26674533cbb740
Shaun Fox <shaun@wheelhouse-studio.com>
2019-07-26 21:43:47 -0700

    Add influencer and news typescript interfaces

---
+++ content/src/components/types/influencer.ts
@@ -0,0 +1,23 @@
+export interface IInfluencer {
+  first_name?: string;
+  last_name?: string;
+  full_name?: string;
+  image_uri?: string;
+  image_attribution?: string;
+  facebook_uri?: string;
+  facebook_display?: string;
+  instagram_uri?: string;
+  instagram_display?: string;
+  youtube_uri?: string;
+  youtube_display?: string;
+  web_uri?: string;
+  web_display?: string;
+  twitch_uri?: string;
+  twitch_display?: string;
+  wikipedia_uri?: string;
+  wikipedia_display?: string;
+  twitter_uri?: string;
+  twitter_display?: string;
+  primary_uri?: string;
+  primary_display?: string;
+}
---
+++ content/src/components/types/news.ts
@@ -0,0 +1,9 @@
+export interface INews {
+  title?: string;
+  description?: string;
+  link?: string;
+  author?: string;
+  source?: string;
+  pubdate?: string;
+  summary?: string;
+}
