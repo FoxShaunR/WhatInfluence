31ff3cf0319addca7786cfe8b1cfb7945d0653f5
Shaun Fox <shaun@wheelhouse-studio.com>
2019-08-01 20:28:23 -0700

    Update LatestNews styling, add influencer route

--- content/src/App.tsx
+++ content/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styles from './App.module.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import InfluencerDetail from './components/InfluencerDetail/InfluencerDetail';
 import LatestNews from './components/LatestNews';
 import Mission from './components/Mission/Mission';
 import SearchResults from './components/SearchResults/SearchResults';
@@ -17,6 +18,7 @@ const App: React.FC = () => {
             <Route exact path="/" component={LatestNews} />
             <Route path="/mission" component={Mission} />
             <Route path="/search" component={SearchResults} />
+            <Route path="/influencer" component={InfluencerDetail} />
           </Switch>
         <Footer />
       </Router>
--- content/src/components/LatestNews/NewsItem.tsx
+++ content/src/components/LatestNews/NewsItem.tsx
@@ -1,6 +1,7 @@
 import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { formatDate } from '../../common/helpers';
 import { ILatestNewsItem } from '../../types/news';
 
@@ -15,21 +16,27 @@ const NewsItem = ({
   pubdate = '',
   influencers,
 }: ILatestNewsItem) => {
-  const { full_name, primary_display} = React.useMemo(() => influencers[0], [influencers]);
+  const {
+    id,
+    full_name,
+    primary_display,
+  } = React.useMemo(() => influencers[0], [influencers]);
   return (
     <div className={styles.main}>
       <div className={styles.headerRow}>
         {/* TODO: update to icon based on source */}
         <FontAwesomeIcon className={styles.icon} icon={faNewspaper} size="4x" />
-        <div className={styles.nameGroup}>
+        <Link to={`/influencer/${id}`} className={styles.nameGroup}>
           <div className={styles.name}>{full_name}</div>
           <div className={styles.handle}>{primary_display}</div>
-        </div>
+        </Link>
         {<div className={styles.pubdate}>{formatDate(new Date(pubdate))}</div>}
       </div>
-      <div className={styles.title}>{title}</div>
-      {/* TODO: additional styling of name */}
-      <div className={styles.description}>{`${description} - ${news_source.name}`}</div>
+      <a rel="noopener noreferrer" target="_blank" href={link} className={styles.description}>
+        <div className={styles.title}>{title}</div>
+        {/* TODO: additional styling of name */}
+        <div className={styles.description}>{`${description} - ${news_source.name}`}</div>
+      </a>
     </div>
   );
 };
