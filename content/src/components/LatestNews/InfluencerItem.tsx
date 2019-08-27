04eb278938c0b1253522a524f4edbeaa8389d843
Shaun Fox <shaun@wheelhouse-studio.com>
2019-08-27 08:43:58 -0700

    Add mobile styling to InfluencerItem

--- content/src/components/LatestNews/InfluencerItem.tsx
+++ content/src/components/LatestNews/InfluencerItem.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import { getInfluencerIconFromURL } from '../../common/iconHelpers';
 import { TPartialInfluencer } from '../../types/influencer';
@@ -15,7 +16,12 @@ const InfluencerItem = ({
   const icon = React.useMemo(() => getInfluencerIconFromURL(primary_uri), [primary_uri]);
   return (
     <div className={styles.main}>
-      <FontAwesomeIcon className={styles.icon} icon={icon} size="4x" />
+      <MediaQuery minWidth={807}>
+        <FontAwesomeIcon className={styles.icon} icon={icon} size="4x" />
+      </MediaQuery>
+      <MediaQuery maxWidth={806}>
+        <FontAwesomeIcon className={styles.icon} icon={icon} size="3x" />
+      </MediaQuery>
       <Link to={`/influencer/${id}`} className={styles.nameGroup}>
         <div className={styles.name}>{primary_display}</div>
         <div className={styles.handle}>{full_name}</div>
